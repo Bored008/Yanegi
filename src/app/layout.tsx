e0cf9fb64bd55668b07483c28e83fe2682f3f712
Bored008 <dahiyahimanshu80@gmail.com>
2026-07-11 19:59:13 +0530

    fix some minor issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export const metadata: Metadata = {
 };
 
 import { SmoothScroll } from "@/components/smooth-scroll";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
@@ -44,6 +45,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <SmoothScroll>{children}</SmoothScroll>
+        <Analytics />
       </body>
     </html>
   );
